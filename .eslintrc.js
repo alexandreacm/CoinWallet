bbc72917bc603f1866b3731e506d4dce11d03c1e
Alexandre Marques <alexandreacm.marques@gmail.coms>
2022-09-11 10:28:40 -0300

    eslint changed.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,32 @@
 module.exports = {
   env: {
-    es2021: true,
+    es6: true,
     node: true,
     jest: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'airbnb/hooks', 'prettier'],
-  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
   parserOptions: {
     project: './tsconfig.json',
   },
-  plugins: ['react', 'react-native', 'prettier', 'react-hooks', '@typescript-eslint'],
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
   rules: {
-    'prettier/prettier': 'error',
-    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
-    'import/prefer-default-export': 'off',
-    'no-param-reassign': 'off',
-    'no-console': ['error', { allow: ['tron'] }],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    quotes: ['error', 'single', { avoidEscape: true }],
     'no-empty-function': 'off',
     '@typescript-eslint/no-empty-function': 'off',
-    'import/no-extraneous-dependencies': [
-      'error',
-      { devDependencies: ['**/*.test.ts', '**/*.spec.ts'] },
-    ],
-    'import/extensions': ['error', 'never', { ignorePackages: true }],
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 };
